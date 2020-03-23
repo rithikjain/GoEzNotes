c44a5d50ed6263a2ceaa4ff885d9b8e49ddd3527
Rithik Jain <rithik.jain3006@gmail.com>
2020-03-23 15:52:19 +0530

    Added pass hashing with bcrypt

--- api/controllers/auth.go
+++ api/controllers/auth.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/rithikjain/TodoApi/api/models"
 	"github.com/rithikjain/TodoApi/api/views"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func RegisterUser() http.HandlerFunc {
@@ -13,13 +15,28 @@ func RegisterUser() http.HandlerFunc {
 		if r.Method == http.MethodPost {
 			data := views.RegisterRequest{}
 			json.NewDecoder(r.Body).Decode(&data)
-			err := models.CreateUser(data.Name, data.Email, data.Password)
+			hashPass, hashErr := HashPassword(data.Password)
+			if hashErr != nil {
+				log.Fatal(hashErr)
+				return
+			}
+			err := models.CreateUser(data.Name, data.Email, hashPass)
 			if err != nil {
 				w.Write([]byte("Error in creating user!"))
 				return
 			}
 			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(data)
+			w.Write([]byte("User Succesfully Created."))
 		}
 	}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
