f1ccca2a5f61eda168b120cb8e4afb906a2a5b56
Rithik Jain <rithik.jain3006@gmail.com>
2020-03-24 18:24:22 +0530

    Added Vendors

--- api/main.go
+++ api/main.go
@@ -29,7 +29,6 @@ func main() {
 // Get the Port from the environment so we can run on Heroku
 func GetPort() string {
 	var port = os.Getenv("PORT")
-	// Set a default port if there is nothing in the environment
 	if port == "" {
 		port = "4747"
 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
